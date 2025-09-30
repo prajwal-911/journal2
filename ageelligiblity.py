79bc2e8b4471b8dca1f8994fd41857114b29da96
prajwal-911 <prajwalawati101@gmail.com>
2025-09-29 22:22:13 -0700

    Create ageelligiblity.py

---
+++ ageelligiblity.py
@@ -0,0 +1,10 @@
+#jornal 2
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+print("\nHello", name + "!")
+
+if age >= 18:
+    print("You are eligible to vote in elections.")
+    
+print("Thank you for using the program!")
